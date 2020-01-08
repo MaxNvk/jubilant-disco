d73e00170218a73e393859b39dc3b018c7b1fe48
Max Novikov <max.vashuk@gmail.com>
2020-01-08 11:34:20 +0200

    made login to account

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ const puppeteer = require('puppeteer');
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-    deviceScaleFactor: 1,
-  });
+  // const navigationPromise = page.waitForNavigation({waitUntil: ['networkidle2'] });
 
-  await page.goto('https://stackoverflow.com/');
-  await page.screenshot({path: `img/${new Date()}.png`});
+    await page.setViewport({
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+    });
+
+  await page.goto('https://stackoverflow.com/users/login');
+  
+  await page.waitForSelector('input#email');
+  await page.type('input#email', '')
+  await page.waitForSelector('input#password');
+  await page.type('input#password', '');
+  
+  // await navigationPromise;
+  
+  await Promise.all([
+    page.keyboard.press("Enter"),
+    page.waitForNavigation({ waitUntil: 'networkidle0' }),
+  ]);
+  
+    await page.screenshot({path: `img/${new Date()}.png`});
 
   await browser.close();
 }
